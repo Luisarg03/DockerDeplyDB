3c5fee5b65e082bcf83c52d4cc6e74d16e1edf7a
Luisarg03 <luis.m.paz.03@gmail.com>
2020-08-27 18:19:27 -0300

    HideInfo

--- SQLServerCampaign/SASCampaignCreate.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE [SASCampaign]
- CONTAINMENT = NONE
- ON  PRIMARY
-( NAME = N'SASCampaign', FILENAME = N'C:\SQLData\SASCampaign.mdf')
- LOG ON
-( NAME = N'SASCampaign_log', FILENAME = N'C:\SQLData\SASCampaign_log.ldf')
-GO
--- init.bat
+++ init.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 echo Iniciando deploy...
 
-if not exist "C:\Docker\SQLServerCampaign\volume" mkdir C:\Docker\SQLServerCampaign\volume
+if not exist "C:\Docker\DockerDeplyDB\SQLServerDB\volume" mkdir C:\Docker\DockerDeplyDB\SQLServerDB\volume
 
 timeout 0
 docker-compose up -d
@@ -10,16 +10,16 @@ timeout 10
 docker container ls
 
 timeout 5
-echo Creando BD SASCampaign
-sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\SQLServerCampaign\SASCampaignCreate.sql
+echo Creando BD
+sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\DockerDeplyDB\SQLServerDB\DB.sql
 
 timeout 5
-echo Creando tablas SASCampaign
-sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\SQLServerCampaign\SASCampaign_Tables.sql -o C:\Docker\SQLServerCampaign\SASCampaign_Tables.txt
+echo Creando tablas
+sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\DockerDeplyDB\SQLServerDB\DB_Tables.sql -o C:\Docker\DockerDeplyDB\SQLServerDB\DB_Tables.txt
 
 timeout 5
-echo Creando Vistas y Procedimientos SASCampaign
-sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\SQLServerCampaign\SASCampaign_Vw_Sp.sql -o C:\Docker\SQLServerCampaign\SASCampaign_Vw_Sp.txt
+echo Creando Vistas y Procedimientos
+sqlcmd -S localhost,1433 -U sa -P Password_01 -i C:\Docker\DockerDeplyDB\SQLServerDB\DB_Vw_Sp.sql -o C:\Docker\DockerDeplyDB\SQLServerDB\DB_Vw_Sp.txt
 
 timeout 0
 echo Deploy finalizado...
